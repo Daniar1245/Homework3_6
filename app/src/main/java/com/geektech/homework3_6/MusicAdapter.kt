98d07b33b46fbf21a07f689821362b8f1a63f410
Daniar1245 <daniarraimkulov123@gmail.com>
2022-10-28 21:50:50 +0600

    commit

--- app/src/main/java/com/geektech/homework3_6/MusicAdapter.kt
+++ app/src/main/java/com/geektech/homework3_6/MusicAdapter.kt
@@ -1,5 +1,6 @@
 package com.geektech.homework3_6
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
